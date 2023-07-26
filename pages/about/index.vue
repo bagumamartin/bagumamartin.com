da361c45fae5b1a8656e8bd0baaba97af7088d6c
BAGUMA MARTIN AMANYA <amanyamartin05@gmail.com>
2023-07-27 00:53:12 +0300

    recommencing SEO

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full h-[70vh] text-center">
+    <div class="text-heading-1 text-primary font-poppins">
+      <h1>Coming Soon</h1>
+    </div>
+    <div class="text-paragraph text-black font-lexend">
+      <p>Page is still under development</p>
+    </div>
+    <div class="w-full flex justify-center mt-[10vh]">
+      <UtilitiesButton
+        class="w-[70vw] rounded-full bg-accent"
+        label="Go back to home page"
+        navigation-path="/"
+      />
+    </div>
+  </div>
+</template>
