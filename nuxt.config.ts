8ab8eb8e7a4bb21b45ff8cfd175411d05eee163c
BAGUMA MARTIN AMANYA <amanyamartin05@gmail.com>
2023-07-28 12:32:06 +0300

    Fix:google analytics works

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "nuxt-icon"],
+  modules: ["@pinia/nuxt", "nuxt-icon", "nuxt-gtag"],
   css: ["~/assets/style/main.scss"],
   postcss: {
     plugins: {
@@ -25,38 +25,14 @@ export default defineNuxtConfig({
   generate: {
     fallback: true, // Uses '404.html' instead of the default '200.html'
   },
-  gtag: {
-    id: process.env.NUXT_PUBLIC_GTAG_ID,
-  },
+  // gtag: {
+  //   id: process.env.NUXT_PUBLIC_GTAG_ID,
+  // },
   app: {
     head: {
       htmlAttrs: {
         lang: "en",
       },
-      // meta: [
-      //   {
-      //     hid: "og:image",
-      //     property: "og:image",
-      //     content: "https://bagumamartin.com/images/social_cards_image.jpg",
-      //   },
-      //   {
-      //     hid: "og:image:secure_url",
-      //     property: "og:image:secure_url",
-      //     content: "https://bagumamartin.com/images/social_cards_image.jpg",
-      //   },
-      //   { charset: "utf-8" },
-      //   {
-      //     name: "viewport",
-      //     content:
-      //       "width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no",
-      //   },
-      //   {
-      //     hid: "description",
-      //     name: "description",
-      //     content:
-      //       "Looking for a professional application developer in Uganda? I offer a wide range of custom software development services, including website, mobile and desktop apps design and development. I am experienced in various major programming languages and platforms, and I can help you create applications that meets your specific needs. Hire me for your project today.",
-      //   },
-      // ],
       title:
         "Software Applications Development Services in Uganda | Hire a Professional Developer",
       link: [
